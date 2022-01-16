2daf2ef850a22405c22f9122ac502be3e4283bfa
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2022-01-16 13:41:28 -0500

    Delete state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { AppContextWrapper } from "./store/AppContext";
 
 const App = () => {
-  return (
-    <AppContextWrapper>
-      <p>todo-app</p>
-    </AppContextWrapper>
-  );
+  return <p>todo-app</p>;
 };
 
 export default App;
